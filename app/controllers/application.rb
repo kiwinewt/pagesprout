55021e3e4c9de51b4fe3a109aab9977a55b07852
Nate Walker <kiwinewt@gmail.com>
2008-08-21 02:05:49 +0000

    Started on User Authentication and Roles.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,14 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '6188a16a9d19044c84f5187344230cd9'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
-  # filter_parameter_logging :password
+  helper :all
+  include AuthenticatedSystem
+  protect_from_forgery
 end
