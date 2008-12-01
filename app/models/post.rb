7f12f3ba8a9effac9ae22d6b61b3e0670df1ba4c
Aupajo <pete@metanation.com>
2008-12-01 15:45:20 +1300

    Updated enabled named_scope to accept limit parameter.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@
 # Copyright:: Copyright (c) 2008 Rocket Boys Ltd
 # License::   BSD Licence, see application root.
 class Post < ActiveRecord::Base
+  named_scope :enabled,  lambda { |*limit| { :conditions => { :enabled => true }, :limit => limit.flatten.first }
+  
   acts_as_ferret :fields => { :title => { :boost => 2 }, :body => {}, :slug_with_spaces => {} },
                  :remote => true,
                  :store_class_name => true
