c1889025b57919e92d9ece98fdfa47c58bb00ba8
Nate Walker <kiwinewt@gmail.com>
2008-12-05 10:51:29 +1300

    Attempted to start unit tests.

--- test/unit/blog_test.rb
+++ test/unit/blog_test.rb
@@ -15,11 +15,48 @@ class BlogTest < ActiveSupport::TestCase
   end
   
   test "enables blog" do
-    assert true
+    blog = blogs(:hidden)
+    assert !blog.enabled?
+    blog.enabled = true
+    assert blog.enabled?
   end
   
   test "disables blog" do
-    assert true
+    blog = blogs(:ala)
+    assert blog.enabled?
+    blog.enabled = false
+    assert !blog.enabled?
+  end
+  
+  test "update blog" do
+    blog = blogs(:ala)
+    old_title = blog.title
+    blog.title = "Not a list apart"
+    assert_not_equal( old_title, blog.title )
+    old_desc = blog.description
+    blog.description = "Not a blog, really..."
+    assert_not_equal( old_desc, blog.description )
+  end
+  
+  test "param is permalink" do
+    blog = blogs(:ala)
+    assert_equal( blog.to_param, blog.permalink )
+  end
+  
+  test "permalink with spaces has spaces" do
+    blog = blogs(:ala)
+    assert blog.permalink_with_spaces.include? " "
+  end
+  
+  test "pages" do
+    blog = blogs(:ala)
+    assert blog.posts?
+    assert blog.enabled_posts_shortlist.count >= 1
+  end
+  
+  test "delete blog" do
+    blog = blogs(:ala)
+    assert blog.destroy
   end
   
 end
